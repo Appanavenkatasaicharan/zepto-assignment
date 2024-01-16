d898b04509b1db7920a84ad623935c5e61f00d1b
Sai Charan <venkatasaicharan.a@gmail.com>
2024-01-16 19:00:18 +0530

    final

--- src/App.js
+++ src/App.js
@@ -1,23 +1,99 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
+import data from './mock-data'
+
 function App() {
+
+  const [users,setUsers] = useState(data);
+  const [selectedUsers,setSelectedUsers] = useState([]);
+  const [highlightedUser,setHighlightedUser] = useState(null);
+  const [name,setName] = useState("");
+  const [isDropdownVisible, setDropdownVisible] = useState(false);
+
+  const handleOnChange = (e)=>{
+    setName(e.target.value);
+  }
+
+  const handleClick = ()=>{
+    setDropdownVisible(true);
+    setHighlightedUser(null)
+  }
+
+  const handleKeyDown = (event)=>{
+    if(event.key === 'Backspace'){
+      if(name===""){
+        setHighlightedUser(selectedUsers[selectedUsers.length-1])
+        document.getElementById('root').click()
+      }
+    }
+  }
+
+  window.onclick = function(event) {
+    if (!event.target.matches('.name-input')) {
+      setDropdownVisible(false);
+    }
+  }
+
+  // window.onkeydown = function(event){
+  //   if(event.key === 'Backspace'&&highlightedUser!=null){
+  //     removeUser(highlightedUser)
+  //     setHighlightedUser(null)
+  //   }
+  // }
+
+  const addUser = (user)=>{
+    setName("")
+    setSelectedUsers((prevSelectedUsers)=>[...prevSelectedUsers,user])
+  }
+
+  const removeUser = (userToRemove)=>{
+    setSelectedUsers((prevSelectedUsers)=>prevSelectedUsers.filter((user)=>userToRemove!==user))
+  }
+
+  const highlightUser = (user)=>{
+    setHighlightedUser(user);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 className='heading'>Pick users</h1>
+      <div className='container'>
+        {/* <div className='chip-container'> */}
+          {
+            selectedUsers.map((user)=>{
+              const style = user===highlightedUser?"chip highlighted":"chip";
+              return (
+                <div className={style} onClick={()=>highlightUser(user)}>
+                  <img src={user.photo} className='chip-photo' alt='profile' />
+                  <p style={{fontSize:'medium',color:'grey'}}>{user.name}</p>
+                  <p className='remove-button' onClick={()=>removeUser(user)}>X</p>
+                </div>
+              )
+            })
+          }
+        {/* </div> */}
+        <div className='dropdown-container'>
+          <input className='name-input' type='text' placeholder='Add new user...' value={name} onChange={handleOnChange} onClick={handleClick} onKeyDown={handleKeyDown}/>
+          { isDropdownVisible &&
+          <div className='dropdown'>
+            {
+              users.map((user)=>{
+                if(!selectedUsers.includes(user)&&user.name.toLowerCase().includes(name.toLowerCase()))
+                return (
+                  <div className='user'  onClick={()=>addUser(user)}>
+                    <img src={user.photo} className='user-photo' alt='profile' />
+                    <p style={{fontWeight:'bold'}}>{user.name}</p>
+                    <p style={{fontSize:'medium',color:'grey'}}>{user.email}</p>
+                  </div>
+                )
+              })
+            }
+          </div>
+          }
+        </div>
+        
+      </div>
     </div>
   );
 }
---
+++ src/mock-data.js
@@ -0,0 +1,86 @@
+
+const data =  [
+ { photo:"https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png",
+        name: "Emily Johnson",
+        email: "emily.johnson@email.com"
+    },
+    { photo:"https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png",
+      name: "Olivia Taylor"
+      , email: "olivia.taylor@email.com"
+    },
+      {photo:"https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png",
+      name: "Ethan Brown"
+      , email: "ethan.brown@email.com"
+    },
+      {photo:"https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png",
+      name: "Daniel Smith"
+      , email: "daniel.smith@email.com"
+    },
+      {photo:"https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png",
+      name: "Sophia Anderson"
+      , email: "sophia.anderson@email.com"
+    },
+      {photo:"https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png",
+      name: "Jackson Miller"
+      , email: "jackson.miller@email.com"
+    },
+      {photo:"https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png",
+      name: "Ava Davis"
+      , email: "ava.davis@email.com"
+    },
+      {photo:"https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png",
+      name: "Liam Wilson"
+      , email: "liam.wilson@email.com"
+    },
+      {photo:"https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png",
+      name: "Isabella Martin"
+      , email: "isabella.martin@email.com"
+    },
+      {photo:"https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png",
+      name: "Mason Jones"
+      , email: "mason.jones@email.com"
+    },
+      {photo:"https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png",
+      name: "Emma White"
+      , email: "emma.white@email.com"
+    },
+      {photo:"https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png",
+      name: "Caleb Harris"
+      , email: "caleb.harris@email.com"
+    },
+      {photo:"https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png",
+      name: "Abigail Thompson"
+      , email: "abigail.thompson@email.com"
+    },
+      {photo:"https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png",
+      name: "Logan Carter"
+      , email: "logan.carter@email.com"
+    },
+      {photo:"https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png",
+      name: "Avery Clark"
+      , email: "avery.clark@email.com"
+    },
+      {photo:"https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png",
+      name: "Benjamin Turner"
+      , email: "benjamin.turner@email.com"
+    },
+      {photo:"https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png",
+      name: "Zoe Lewis"
+      , email: "zoe.lewis@email.com"
+    },
+      {photo:"https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png",
+      name: "Henry Ward"
+      , email: "henry.ward@email.com"
+    },
+      {photo:"https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png",
+      name: "Chloe Foster"
+      , "email": "chloe.foster@email.com"
+    },
+      {photo:"https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png",
+      name: "Mia Hughes"
+      , email: "mia.hughes@email.com"
+    }
+]
+
+export default data;
+  
